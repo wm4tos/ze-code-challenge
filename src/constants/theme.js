1f02dd2c4f9d5723972294a611fb3cb5eba741ff
Wesley Matos <wrickee@gmail.com>
2020-10-16 22:08:15 -0300

    style: add theme constants

---
+++ src/constants/theme.js
@@ -0,0 +1,80 @@
+export const COLORS = {
+  yellow: '#FFC500',
+  light_yellow: '#F9D238',
+  dark_yellow: '#F9C104',
+  orange: '#F39000',
+  white: '#FFFFFF',
+  medium_white: '#FAFAFA',
+  off_white: '#F5F5F5',
+  black: '#000000',
+  black_menu: '#333',
+  c_gray: '#ccc',
+  gray: '#333',
+  light_gray: '#E6E6E6',
+  medium_gray: '#666',
+  dark_gray: '#999',
+  red: '#F54337',
+  green: '#30D07B',
+  blue: '#1A73E8',
+  facebook: '#3559A4',
+  dark_facebook: '#2C4B8C',
+  whatsapp_green: '#075E54',
+  snowflake_blue: '#1B91F7',
+  snow_blue: '#BEE0FD',
+  snow_white: '#EEF7FF',
+  light_shadow: 'rgba(0, 0, 0, 0.15)',
+  medium_shadow: 'rgba(0, 0, 0, 0.25)',
+  dark_shadow: 'rgba(0, 0, 0, 0.5)',
+  beige: '#FFF0B6',
+}
+
+export const FONT_SIZES = {
+  small: 12,
+  medium: 14,
+  large: 16,
+  extra_large: 24,
+}
+
+export const LINE_HEIGHTS = {
+  small: 18,
+  medium: 20,
+  large: 40,
+}
+
+export const BOX_SHADOW = {
+  extra_small: '0 2px 4px 0',
+  small: '0 6px 14px 0',
+}
+
+export const SCREEN_SIZES = {
+  minimum_width: 0.02,
+  old_phones: 250,
+  phones: 576,
+  tablets: 768,
+  desktops: 992,
+  large_desktops: 1200,
+}
+
+export const MARGIN_PADDING = {
+  extra_small: 4,
+  tiny: 8,
+  small: 12,
+  medium: 16,
+  large: 20,
+  extra_large: 24,
+}
+
+export const FONT_FAMILIES = {
+  roboto: 'Roboto',
+}
+
+export const FONT_WEIGHT = {
+  regular: 400,
+  medium: 500,
+}
+
+export const ANIMATION_DURATION = 300
+export const ANIMATION_TIMING = 'cubic-bezier(0.75, 0.02, 0.5, 1)'
+export const ICON_SIZE = 24
+export const CONTAINER_WIDTH = 1200
+export const HEADER_HEIGHT = 72
