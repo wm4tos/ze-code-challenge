b0e3e5898cea786179a4525587dc0b90be5d35d0
Wesley Matos <wrickee@gmail.com>
2020-10-17 10:58:56 -0300

    chore: create tests config

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['js', 'jsx', 'json'],
+  setupFilesAfterEnv: [
+    '<rootDir>/src/setupTests.js',
+  ],
+}
