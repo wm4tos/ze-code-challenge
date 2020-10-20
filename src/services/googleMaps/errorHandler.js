fc2f551138b98a53bd2a65f08c9eea9c55d69681
Wesley Matos <wrickee@gmail.com>
2020-10-20 11:31:13 -0300

    refactor: separe responsabilities of error handler

--- src/services/googleMaps/errorHandler.js
+++ src/services/googleMaps/errorHandler.js
@@ -1,19 +1,4 @@
-const MESSAGES = {
-  tryAgainMsg: 'Por favor, tente novamente',
-  contactMsg: 'Se o erro persistir, contate o criador da aplicação',
-}
-
-export const ERRORS = {
-  OVER_DAILY_LIMIT: `Limite diário de chamadas excedido. ${MESSAGES.tryAgainMsg} amanhã. ${MESSAGES.contactMsg}.`,
-  OVER_QUERY_LIMIT: `Limite de chamadas excedido. ${MESSAGES.tryAgainMsg}. ${MESSAGES.contactMsg}.`,
-  REQUEST_DENIED: `Requisição negada. ${MESSAGES.tryAgainMsg}. ${MESSAGES.contactMsg}.`,
-  UNKNOWN_ERROR: `Houve um problema no servidor. ${MESSAGES.tryAgainMsg}. ${MESSAGES.contactMsg}.`,
-  default: `Houve um erro. ${MESSAGES.tryAgainMsg}. ${MESSAGES.contactMsg}.`,
-}
-
-export const WARNINGS = {
-  ZERO_RESULTS: 'Nenhum resultado encontrado para sua busca.',
-}
+import { ERRORS, WARNINGS } from './messages'
 
 export default ({ message }) => {
   const isWarning = message in WARNINGS
