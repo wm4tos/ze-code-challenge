c02b2e6f0f12a8e7232e88567da640aee128b369
Wesley Matos <wrickee@gmail.com>
2020-10-26 20:25:10 -0300

    style: include possitiblity of change icon color

--- src/components/atoms/icon/index.jsx
+++ src/components/atoms/icon/index.jsx
@@ -1,9 +1,12 @@
+import { COLORS } from 'constants/theme'
 import PropTypes from 'prop-types'
 import React, { useEffect, useState } from 'react'
 import LazyLoad from 'react-lazyload'
 import { toPascalCase } from 'services/utils/string'
 
-const Icon = ({ name, size }) => {
+import * as styles from './styles'
+
+const Icon = ({ name, size, color }) => {
   const [icon, setIcon] = useState(() => <div />)
 
   useEffect(() => {
@@ -15,7 +18,7 @@ const Icon = ({ name, size }) => {
     const getter = module => {
       const Icon = module[iconName]
 
-      return <Icon size={size} />
+      return <Icon size={size} css={styles.changeColor(color)} />
     }
 
     import(`react-icons/${lowerCaseGroup}/index.js`).then(getter).then(setIcon)
@@ -27,10 +30,12 @@ const Icon = ({ name, size }) => {
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
   size: PropTypes.number,
+  color: PropTypes.string,
 }
 
 Icon.defaultProps = {
   size: 16,
+  color: COLORS.black
 }
 
 export default Icon
---
+++ src/components/atoms/icon/styles.js
@@ -0,0 +1,6 @@
+import { css } from '@emotion/core'
+
+export const changeColor = (color) => css`
+  fill: ${color};
+  color: ${color};
+`
