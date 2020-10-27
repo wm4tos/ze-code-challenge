47cb033eb91d86edec1bcdd89b2fbbdc95f09da8
Wesley Matos <wrickee@gmail.com>
2020-10-26 22:15:58 -0300

    chore: add NODE_ENV to exports

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
 export const { __GOOGLEAPI__ = '' } = process.env
 export const { __API__ = '' } = process.env
 export const { __GOOGLEKEY__ = '' } = process.env
+export const { NODE_ENV = '' } = process.env
