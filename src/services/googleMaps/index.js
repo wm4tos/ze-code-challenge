aeb6e5ae7179f4ef7e840b1d1a135b5937d7a958
Wesley Matos <wrickee@gmail.com>
2020-10-28 13:30:55 -0300

    refactor: export mapper functions individually

--- src/services/googleMaps/index.js
+++ src/services/googleMaps/index.js
@@ -3,12 +3,12 @@ import { __GOOGLEKEY__ } from 'config'
 import api from './api'
 import errorHandler from './errorHandler'
 import getter from './getter'
-import mapper from './mapper'
 
 const get = async (address) => getter(api(), { key: __GOOGLEKEY__, address })
 
 export {
   errorHandler,
   get,
-  mapper,
 }
+
+export * from './mapper'
