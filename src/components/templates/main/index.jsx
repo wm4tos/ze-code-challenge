e6ce635b08727a9e731baff726035fca52bfde8b
Wesley Matos <wrickee@gmail.com>
2020-10-28 13:19:18 -0300

    refactor: control address value internally

--- src/components/templates/main/index.jsx
+++ src/components/templates/main/index.jsx
@@ -1,20 +1,25 @@
 import { Creators } from 'ducks/address'
 import Header from 'organisms/header'
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useDebounce } from 'services/utils/operators'
 
 import * as styles from './styles'
 
 const MainTemplate = () => {
-  const address = useSelector((state) => state.address)
+  const [address, setAddress] = useState('')
 
   const dispatch = useDispatch()
 
-  const handleChange = (v) => dispatch(Creators.setAddress(v))
+  const debouncedAddress = useDebounce(address, 500)
+
+  useEffect(() => {
+    dispatch(Creators.setAddress(debouncedAddress))
+  }, [debouncedAddress])
 
   return (
     <div css={styles.containerBaseStyle}>
-      <Header onChange={handleChange} value={address} />
+      <Header onChange={setAddress} value={address} />
     </div>
   )
 }
