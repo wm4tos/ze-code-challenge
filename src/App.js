a70424a78a1bfa0dce42a838a92b79892c2c099c
Wesley Matos <wrickee@gmail.com>
2020-10-28 23:55:01 -0300

    feat: use router in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Template from 'templates/main'
 
+import Router from './router'
+
 const App = () => (
-  <>
-    <Template />
-  </>
+  <Template>
+    <Router />
+  </Template>
 )
 
 export default App
