132b9481a490315f7ef99a948a21bcddd3b4339e
Wesley Matos <wrickee@gmail.com>
2020-10-29 12:40:26 -0300

    style: write company card style

---
+++ src/components/organisms/company-card/styles.js
@@ -0,0 +1,15 @@
+import { css } from '@emotion/core'
+import { COLORS, FONT_SIZES } from 'constants/theme'
+
+export const cardStyle = css`
+  max-width: 150px;
+`
+
+export const titleStyle = css`
+  font-size: ${FONT_SIZES.large}pt;
+`
+
+export const addressStyle = css`
+  font-size: ${FONT_SIZES.small}pt;
+  color: ${COLORS.dark_gray};
+`
