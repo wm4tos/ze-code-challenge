c46a83ccc0a4d67d0c5da79b4579f8d6528fefec
Wesley Matos <wrickee@gmail.com>
2020-10-29 12:32:22 -0300

    refactor: receive containerStyle

--- src/components/atoms/card/index.jsx
+++ src/components/atoms/card/index.jsx
@@ -3,14 +3,19 @@ import React from 'react'
 
 import * as styles from './styles'
 
-const Card = ({ children, ...props }) => (
-  <div css={styles.cardBaseStyle} {...props}>
+const Card = ({ children, containerStyle, ...props }) => (
+  <div css={[styles.cardBaseStyle, containerStyle]} {...props}>
     {children}
   </div>
 )
 
 Card.propTypes = {
   children: PropTypes.oneOfType([PropTypes.array, PropTypes.element]).isRequired,
+  containerStyle: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+}
+
+Card.defaultProps = {
+  containerStyle: {},
 }
 
 export default Card
