291836ad638d354becca4c50c0753b50c47ebaa0
Wesley Matos <wrickee@gmail.com>
2020-10-29 12:33:42 -0300

    styçe: add medium padding

--- src/components/atoms/card/styles.js
+++ src/components/atoms/card/styles.js
@@ -1,8 +1,9 @@
 import { css } from '@emotion/core'
-import { BOX_SHADOW, COLORS } from 'constants/theme'
+import { BOX_SHADOW, COLORS, MARGIN_PADDING } from 'constants/theme'
 
 export const cardBaseStyle = css`
   border-radius: 8px;
   background-color: ${COLORS.white};
   box-shadow: ${BOX_SHADOW.extra_small} ${COLORS.light_shadow};
+  padding: ${MARGIN_PADDING.medium}px;
 `
