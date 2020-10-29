da67334e62de5009398bf7f74df7b855ae9acb5f
Wesley Matos <wrickee@gmail.com>
2020-10-29 11:14:40 -0300

    chore: create capitalize util function

--- src/services/utils/string.js
+++ src/services/utils/string.js
@@ -1,3 +1,5 @@
 export const clearAndUpper = (text) => text.replace(/-/, '').toUpperCase()
 
 export const toPascalCase = (text) => text.replace(/(^\w|-\w)/g, clearAndUpper)
+
+export const capitalize = (text) => text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
