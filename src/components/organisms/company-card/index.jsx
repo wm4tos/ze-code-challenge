bc7e6a845dec6aeb648988526569327e27028ea1
Wesley Matos <wrickee@gmail.com>
2020-11-02 00:14:11 -0300

    refactor: don't receive company inside a specific prop

--- src/components/organisms/company-card/index.jsx
+++ src/components/organisms/company-card/index.jsx
@@ -11,51 +11,53 @@ import {
 
 import * as styles from './styles'
 
-const CompanyCard = ({ company }) => (
+const CompanyCard = ({
+  officialName, address, deliveryTypes, paymentMethods, phone,
+}) => (
   <Card containerStyle={styles.cardStyle}>
-    <h1 css={styles.titleStyle}>{company.officialName}</h1>
-    <p css={styles.addressStyle}>{formatAddress(company.address)}</p>
-    <IconList items={mapDeliveryTypes(company.deliveryTypes)} />
-    <IconList items={mapPaymentMethods(company.paymentMethods)} />
-    <PhoneLink phoneNumber={company.phone.phoneNumber} />
+    <h1 css={styles.titleStyle}>{officialName}</h1>
+    <p css={styles.addressStyle}>{formatAddress(address)}</p>
+    <IconList items={mapDeliveryTypes(deliveryTypes)} />
+    <IconList items={mapPaymentMethods(paymentMethods)} />
+    <PhoneLink phoneNumber={phone.phoneNumber} />
   </Card>
 )
 
-CompanyCard.propTypes = {
-  company: PropTypes.shape({
-    officialName: PropTypes.string.isRequired,
-    address: PropTypes.shape({
-      address1: PropTypes.string.isRequired,
-      address2: PropTypes.string,
-      number: PropTypes.string.isRequired,
-      city: PropTypes.string.isRequired,
-      province: PropTypes.string.isRequired,
-      zip: PropTypes.string.isRequired,
-      coordinates: PropTypes.string.isRequired,
-    }),
-    deliveryTypes: PropTypes.arrayOf(
-      PropTypes.shape({
-        pocDeliveryTypeId: PropTypes.string.isRequired,
-        deliveryTypeId: PropTypes.string.isRequired,
-        price: PropTypes.number,
-        title: PropTypes.string.isRequired,
-        subtitle: PropTypes.string.isRequired,
-        active: PropTypes.bool.isRequired,
-      }),
-    ),
-    paymentMethods: PropTypes.arrayOf(
-      PropTypes.shape({
-        active: PropTypes.bool.isRequired,
-        paymentMethodId: PropTypes.string.isRequired,
-        pocPaymentMethodId: PropTypes.string.isRequired,
-        subtitle: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-      }),
-    ),
-    phone: PropTypes.shape({
-      phoneNumber: PropTypes.string.isRequired,
+export const CompanyCardProps = {
+  officialName: PropTypes.string.isRequired,
+  address: PropTypes.shape({
+    address1: PropTypes.string.isRequired,
+    address2: PropTypes.string,
+    number: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    province: PropTypes.string.isRequired,
+    zip: PropTypes.string.isRequired,
+    coordinates: PropTypes.string.isRequired,
+  }).isRequired,
+  deliveryTypes: PropTypes.arrayOf(
+    PropTypes.shape({
+      pocDeliveryTypeId: PropTypes.string.isRequired,
+      deliveryTypeId: PropTypes.string.isRequired,
+      price: PropTypes.number,
+      title: PropTypes.string.isRequired,
+      subtitle: PropTypes.string.isRequired,
+      active: PropTypes.bool.isRequired,
     }).isRequired,
+  ).isRequired,
+  paymentMethods: PropTypes.arrayOf(
+    PropTypes.shape({
+      active: PropTypes.bool.isRequired,
+      paymentMethodId: PropTypes.string.isRequired,
+      pocPaymentMethodId: PropTypes.string.isRequired,
+      subtitle: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+  phone: PropTypes.shape({
+    phoneNumber: PropTypes.string.isRequired,
   }).isRequired,
 }
+
+CompanyCard.propTypes = CompanyCardProps
 
 export default CompanyCard
