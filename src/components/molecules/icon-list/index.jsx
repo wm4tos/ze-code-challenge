3006d9d0637d29d4d1aafcbcea6d40e9a9731145
Wesley Matos <wrickee@gmail.com>
2020-11-02 12:34:19 -0300

    refactor: use list component on icon-list

--- src/components/molecules/icon-list/index.jsx
+++ src/components/molecules/icon-list/index.jsx
@@ -1,18 +1,17 @@
+import List from 'atoms/list'
 import IconTooltip from 'molecules/icon-tooltip'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 import * as styles from './styles'
 
-const IconList = ({ items, alignment }) => (
-  <ul css={styles.getListBaseStyle(alignment)}>
-    {items.map((item) => (
-      <li key={item.key}>
-        <IconTooltip iconName={item.icon} text={item.label} position="top middle" />
-      </li>
-    ))}
-  </ul>
-)
+const IconList = ({ items, alignment }) => {
+  const mapper = ({ icon: iconName, label: text, ...item }) => ({
+    iconName, text, position: 'right middle', ...item,
+  })
+
+  return <List items={items} containerStyle={styles.getListBaseStyle(alignment)} Component={IconTooltip} itemMapper={mapper} />
+}
 
 IconList.propTypes = {
   items: PropTypes.arrayOf(PropTypes.shape({
--- src/components/molecules/icon-list/styles.js
+++ src/components/molecules/icon-list/styles.js
@@ -1,9 +1,6 @@
 import { css } from '@emotion/core'
 
 export const getListBaseStyle = (justify) => css`
-  list-style-type: none;
   display: flex;
   justify-content: ${justify};
-  padding: 0;
-  margin: 0;
 `
