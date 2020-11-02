75159a9dbcddc283f8e77be9c95723e74e33b2a4
Wesley Matos <wrickee@gmail.com>
2020-11-02 17:12:09 -0300

    test: fix test of card component

--- src/components/atoms/card/card.spec.jsx
+++ src/components/atoms/card/card.spec.jsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/core'
 import { render } from '@testing-library/react'
 import React from 'react'
 
@@ -5,14 +6,25 @@ import Card from './index'
 
 describe('<Card />', () => {
   let expected
+  let dataText
 
   beforeEach(() => {
-    expected = <div>Random data</div>
+    dataText = 'Random data'
+    expected = <div>{dataText}</div>
   })
 
   it('should render expected data', () => {
     const { baseElement } = render(<Card>{expected}</Card>)
 
     expect(baseElement).toBeInTheDocument(expected)
+  })
+
+  it('should render expected data', () => {
+    const expectedStyle = `
+      background-color: black;
+    `
+    const { getByText } = render(<Card containerStyle={css`${expectedStyle}`}>{expected}</Card>)
+
+    expect(getByText(dataText).parentElement).toHaveStyle(expectedStyle)
   })
 })
